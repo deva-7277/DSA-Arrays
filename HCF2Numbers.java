9a182443be350e137a5ba53bf89cdd2b1636a845
deva-7277 <81567232+deva-7277@users.noreply.github.com>
2022-09-11 22:00:13 +0530

    Add files via upload

---
+++ HCF2Numbers.java
@@ -0,0 +1,24 @@
+package loops;
+
+import java.util.Scanner;
+
+public class HCF2Numbers {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter 1st number");
+		int num1 = sc.nextInt();
+		System.out.println("Enter 2nd number");
+		int num2 = sc.nextInt();
+		
+		int hcf=0;
+		for(int i=1; i<=num1 && i<=num2; i++) {
+			if(num1%i == 0 && num2%i==0) {
+				hcf = i;
+			}
+		}
+		System.out.println("HCF of "+ num1 +" and "+ num2 +" is "+ hcf);
+		sc.close();
+	}
+
+}
